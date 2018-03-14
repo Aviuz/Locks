6bc40cd348af5c78c990dbf70f3c14379127d3d9
Avius <lion29.box@gmail.com>
2018-03-14 16:28:45 +0100

    ref changes

--- Compile and Run.bat
+++ Compile and Run.bat
@@ -1,36 +1,36 @@
 :: ========= Build ==========
 
-devenv "D:\Git Repository\Locks\Source\Locks.sln" /build Debug
+devenv "Source\Locks.sln" /build Debug
 
 
 
 :: ========= Copy ==========
  
-rd "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks" /s /q
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks"
+rd "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks" /s /q
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks"
 
 :: About
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\About"
-xcopy "D:\Git Repository\Locks\About\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\About" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\About"
+xcopy "About\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\About" /e
 
 :: Assemblies
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Assemblies"
-xcopy "D:\Git Repository\Locks\Assemblies\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Assemblies" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Assemblies"
+xcopy "Assemblies\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Assemblies" /e
 
 :: Defs 
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Defs"
-xcopy "D:\Git Repository\Locks\Defs\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Defs" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Defs"
+xcopy "Defs\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Defs" /e
 
 :: Languages
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Languages"
-xcopy "D:\Git Repository\Locks\Languages\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Languages" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Languages"
+xcopy "Languages\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Languages" /e
 
 :: Textures
-mkdir "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Textures"
-xcopy "D:\Git Repository\Locks\Textures\*.*" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\Textures" /e
+mkdir "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Textures"
+xcopy "Textures\*.*" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\Textures" /e
 
 :: changelog.txt
-copy "D:\Git Repository\Locks\changelog.txt" "D:\Games\SteamLibrary\SteamApps\common\RimWorld\Mods\Locks\changelog.txt"
+copy "changelog.txt" "D:\Program Files\Steam\steamapps\common\RimWorld\Mods\Locks\changelog.txt"
 
 
 
