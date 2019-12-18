14ae0ac1098a99e392d43fb577208347c8a33e7e
Aviuz <Aviuz@users.noreply.github.com>
2019-12-18 20:28:44 +0100

    changed file extension

--- 2. Release.bat
+++ 2. Release.bat
@@ -7,7 +7,7 @@ SET steam_description=https://steamcommunity.com/sharedfiles/itemedittext/?id=11
 SET ludeon_thread=https://ludeon.com/forums/index.php?topic=35913.0
 
 SET target_directory=D:\Program Files\Steam\steamapps\common\RimWorld\Mods\%mod_name%
-SET zip_directory=C:\Users\avius\Desktop\%mod_name%.zip
+SET zip_directory=C:\Users\avius\Desktop\%mod_name%.rar
 
 
 :: ========= Zip archive ==========
