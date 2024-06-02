5f00c3c445c16cdb6319e4c9481f27d8cd27b295
Hazzer <warlocksforge@gmail.com>
2024-06-02 12:58:45 +0200

    2.9.2 fixes (#59)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 </p>
 <p align="center">
   <a href="https://github.com/Aviuz/Locks/releases">
-    <img src="https://img.shields.io/badge/version-2.9.1-blue.svg?style=flat" alt="v2.9.1" />
+    <img src="https://img.shields.io/badge/version-2.9.2-blue.svg?style=flat" alt="v2.9.2" />
   </a>
 </p>
 
--- Source/CompatibilityPatches/DoorsExpanded.cs
+++ Source/CompatibilityPatches/DoorsExpanded.cs
@@ -10,38 +10,19 @@ using Verse;
 
 namespace Locks.CompatibilityPatches
 {
-    class DoorsExpanded
+  class DoorsExpanded
+  {
+    public static void Init()
     {
-        public static void Init()
-        {
-            var mod = LoadedModManager.RunningMods.FirstOrDefault(m => m.Name == "Doors Expanded (Dev)" || m.Name == "Doors Expanded");
-            if (mod != null)
-            {
-                var doorDef = AccessTools.Method("DoorsExpanded.Building_DoorExpanded:PawnCanOpen");
-                if (doorDef != null)
-                {
-                    var harmony = new Harmony("Harmony_Locks_DoorsExpanded");
-                    harmony.Patch(doorDef, transpiler: new HarmonyMethod(typeof(DoorsExpanded).GetMethod("DoorsExpandedTranspiler")));
-                    Log.Message("Locks: Doors expanded patched");
-                }
-                else
-                {
-                    Log.Error("Locks: found doors expanded but can't find door expanded building def");
-                }
-            }
-            else
-            {
-                Log.Message("Locks: Doors expanded not found ");
-            }
-        }
-
-        public static IEnumerable<CodeInstruction> DoorsExpandedTranspiler(ILGenerator gen, MethodBase mBase, IEnumerable<CodeInstruction> instr)
-        {
-            yield return new CodeInstruction(OpCodes.Ldarg_0);
-            yield return new CodeInstruction(OpCodes.Ldarg_1);
-            yield return new CodeInstruction(OpCodes.Call, typeof(LockUtility).GetMethod("PawnCanOpen"));
-            yield return new CodeInstruction(OpCodes.Ret);
-        }
-
+      var mod = LoadedModManager.RunningMods.FirstOrDefault(m => m.Name == "Doors Expanded (Dev)" || m.Name == "Doors Expanded");
+      if (mod != null)
+      {
+        Log.Message("Locks: Doors expanded found");
+      }
+      else
+      {
+        Log.Message("Locks: Doors expanded not found ");
+      }
     }
+  }
 }
