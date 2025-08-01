f3a329e094c2a19523eea56be0b9074faad69184
Hazzer <warlocksforge@gmail.com>
2025-08-01 22:20:57 +0200

    3.1.1

--- Source/Initialization.cs
+++ Source/Initialization.cs
@@ -7,14 +7,14 @@ namespace Locks
   [StaticConstructorOnStartup]
   public static class Initialization
   {
-    public const string version = "3.1.0";
+    public const string VERSION = "3.1.1";
     static Initialization()
     {
       HPatcher.Init();
       ClutterStructure.Init();
       DoorsExpanded.Init();
       SoS2.Init();
-      Log.Message($"Locks version: {version} loaded.");
+      Log.Message($"Locks version: {VERSION} loaded.");
     }
   }
 }
